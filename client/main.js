7e432d7c2f2dc8a0bf8e63291c64a17fcdaf567f
Konstantin <x-stels@bk.ru>
2017-05-22 04:41:37 +0300

    Добавлены необходимые зависимости + некоторые работы по клиенту

---
+++ client/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Konstantin on 22.05.2017.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import App from './components/App.jsx';
+
+ReactDOM.render(
+    //<App />,
+    <h1>Заметки</h1>
+    document.getElementById('mount-point')
+);
